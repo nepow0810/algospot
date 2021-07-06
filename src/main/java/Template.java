5f139dd89c71dc03ec577566114fe6925fe73b15
nepow <gks5428@naver.com>
2021-07-06 22:47:10 +0900

    gitignore 수정 및 그래들 프로젝트 추가

---
+++ src/main/java/Template.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Template {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int cases = scanner.nextInt();
+    while (cases-- > 0) {
+
+    }
+  }
+}
